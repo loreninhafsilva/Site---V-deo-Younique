343a8966c8be4db57fcfbd115d1c753f8dd778c0
loreninhafsilva <cl202243@g.unicamp.br>
2024-04-09 17:41:56 -0300

    login e cadastro finalizado

--- js/script-login.js
+++ js/script-login.js
@@ -1,12 +1,21 @@
 var btnSignin = document.querySelector("#signin");
 var btnSignup = document.querySelector("#signup");
- 
+var contentFirst = document.querySelector(".first-content");
+var contentSecond = document.querySelector(".second-content");
 var body = document.querySelector("body");
 
 btnSignin.addEventListener("click", function (){
-    body.className = 'sign-in-js'
+    if (window.innerWidth <= 850) {
+        contentFirst.style.display = "none";
+        contentSecond.style.display = "flex";
+    }
+    body.className = 'sign-in-js';
 })
 
 btnSignup.addEventListener("click", function (){
-    body.className = 'sign-up-js'
+    if (window.innerWidth <= 850) {
+        contentFirst.style.display = "flex";
+        contentSecond.style.display = "none";
+    }
+    body.className = 'sign-up-js';
 })
